beaa2eeaa6ba41b8639d0c3c9d3b0462dc7a817f
mukulsingh2390 <mukulsingh9146@gmail.com>
2026-02-19 18:20:03 +0530

    add final code6

--- react-vite-web/src/pages/Home.jsx
+++ react-vite-web/src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Hero from "../components/Hero";
+import Hero from "../components/Hero.jsx";
 import OurServices from "../sub-pages/OurServices.jsx";
 import Numbers from "../sub-pages/Numbers.jsx";
 import Industries from "../sub-pages/Industries.jsx";
 import Testimonials from "../sub-pages/Testimonials.jsx";
-import Contact from "./Contact";
+import Contact from "./Contact.jsx";
 
 const Home = () => {
   return (
