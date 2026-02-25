2d9833ba05d5342dab949e30600c2861e907878e
mukulsingh2390 <mukulsingh9146@gmail.com>
2026-02-25 18:31:58 +0530

    your update message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero.jsx";
 import OurServices from "../sub-pages/OurServices.jsx";
 import Numbers from "../sub-pages/Numbers.jsx";
 import Industries from "../sub-pages/Industries.jsx";
-import Partners from "../sub-pages/Partners.jsx";
+import Partners from "../sub-pages/Partners";
 import Testimonials from "../sub-pages/Testimonials.jsx";
 import Contact from "./Contact.jsx";
 
